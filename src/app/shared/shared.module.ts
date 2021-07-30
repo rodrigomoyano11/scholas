608ffa217817b48f264aad7a72390ccc0fa51e8e
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2021-07-30 18:21:06 -0300

    ✨ Se creó el container "not-found" en el módulo "shared"

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 
 import { SharedRoutingModule } from './shared-routing.module'
+import { NotFoundComponent } from './containers/not-found/not-found.component';
+import { LayoutComponent } from './containers/layout/layout.component'
 
 @NgModule({
-  declarations: [],
+  declarations: [NotFoundComponent, LayoutComponent],
   imports: [CommonModule, SharedRoutingModule]
 })
 export class SharedModule {}
