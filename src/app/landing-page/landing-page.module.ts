9000f08becc9294299ff0de14387f4cff2f3a8f4
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2021-07-30 18:00:29 -0300

    ✨ Se creó el container "projects" en el módulo "landing-page"

--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common'
 
 import { LandingPageRoutingModule } from './landing-page-routing.module'
 import { HomeComponent } from './containers/home/home.component'
+import { ProjectsComponent } from './containers/projects/projects.component'
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, ProjectsComponent],
   imports: [CommonModule, LandingPageRoutingModule]
 })
 export class LandingPageModule {}
