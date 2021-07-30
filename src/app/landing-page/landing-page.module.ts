67e812f627db3e65ae1568914341c2793fc44e80
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2021-07-30 18:03:21 -0300

    ✨ Se creó el container "news" en el módulo "landing-page"

--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common'
 import { LandingPageRoutingModule } from './landing-page-routing.module'
 import { HomeComponent } from './containers/home/home.component'
 import { ProjectsComponent } from './containers/projects/projects.component'
+import { NewsComponent } from './containers/news/news.component'
 
 @NgModule({
-  declarations: [HomeComponent, ProjectsComponent],
+  declarations: [HomeComponent, ProjectsComponent, NewsComponent],
   imports: [CommonModule, LandingPageRoutingModule]
 })
 export class LandingPageModule {}
