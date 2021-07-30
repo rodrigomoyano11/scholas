e9d62b6a71080e3f80b37082f104851043373cd6
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2021-07-30 18:09:21 -0300

    ✨ Se creó el container "donors" en el módulo "admin"

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common'
 import { AdminRoutingModule } from './admin-routing.module'
 import { HomeComponent } from './containers/home/home.component'
 import { AdminsComponent } from './containers/admins/admins.component'
+import { DonorsComponent } from './containers/donors/donors.component'
 
 @NgModule({
-  declarations: [HomeComponent, AdminsComponent],
+  declarations: [HomeComponent, AdminsComponent, DonorsComponent],
   imports: [CommonModule, AdminRoutingModule]
 })
 export class AdminModule {}
