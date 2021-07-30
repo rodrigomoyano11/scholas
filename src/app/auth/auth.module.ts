23efdb63b141b015632d0225caa801ca51ea7031
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2021-07-30 18:11:13 -0300

    ✨ Se creó el container "register" en el módulo "auth"

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 
 import { AuthRoutingModule } from './auth-routing.module'
+import { RegisterComponent } from './containers/register/register.component';
+import { LoginComponent } from './containers/login/login.component'
 
 @NgModule({
-  declarations: [],
+  declarations: [RegisterComponent, LoginComponent],
   imports: [CommonModule, AuthRoutingModule]
 })
 export class AuthModule {}
