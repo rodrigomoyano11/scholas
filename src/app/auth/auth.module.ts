9c58244195ad6790955afbe4c2fcabf2bcafc053
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2021-07-30 19:04:59 -0300

    ✨ Se creó el container "home" en el módulo "auth"

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,9 +5,15 @@ import { RegisterComponent } from './containers/register/register.component'
 import { LoginComponent } from './containers/login/login.component'
 import { ForgotPasswordComponent } from './containers/forgot-password/forgot-password.component'
 import { SharedModule } from '../shared/shared.module'
+import { HomeComponent } from './containers/home/home.component'
 
 @NgModule({
-  declarations: [RegisterComponent, LoginComponent, ForgotPasswordComponent],
+  declarations: [
+    RegisterComponent,
+    LoginComponent,
+    ForgotPasswordComponent,
+    HomeComponent
+  ],
   imports: [CommonModule, AuthRoutingModule, SharedModule]
 })
 export class AuthModule {}
