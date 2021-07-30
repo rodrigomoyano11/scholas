67b977735ad32648dca9da0d0c92b024f6ccea8a
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2021-07-30 14:18:43 -0300

    ⚙️ Se realizó la configuración de "stylelint"

---
+++ .stylelintrc.js
@@ -0,0 +1,18 @@
+const sortOrderSmacss = require("stylelint-config-property-sort-order-smacss/generate");
+
+module.exports = {
+  extends: ["stylelint-config-standard"],
+  plugins: ["stylelint-order"],
+  rules: {
+    "no-eol-whitespace": [true, { ignore: ["empty-lines"] }],
+    "no-empty-source": null,
+    "declaration-empty-line-before": [
+      "always",
+      {
+        except: ["first-nested"],
+        ignore: ["after-declaration"],
+      },
+    ],
+    "order/properties-order": [sortOrderSmacss({ emptyLineBefore: "always" })],
+  },
+};
