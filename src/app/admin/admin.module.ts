6b4fd69961a5b5e6053b342e13c8b1c3f389e966
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2021-07-30 18:07:38 -0300

    ✨ Se creó el container "admins" en el módulo "admin"

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common'
 
 import { AdminRoutingModule } from './admin-routing.module'
 import { HomeComponent } from './containers/home/home.component'
+import { AdminsComponent } from './containers/admins/admins.component'
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, AdminsComponent],
   imports: [CommonModule, AdminRoutingModule]
 })
 export class AdminModule {}
---
+++ src/app/admin/containers/admins/admins.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-admins',
+  templateUrl: './admins.component.html',
+  styleUrls: ['./admins.component.css']
+})
+export class AdminsComponent {}
