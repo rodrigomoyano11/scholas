ec50c8ac369ca2fe831adcc5dd93c497d4756770
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2021-07-30 19:00:25 -0300

    ✨ Se creó el componente "footer" en el módulo "shared"

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,9 +4,15 @@ import { SharedRoutingModule } from './shared-routing.module'
 import { LayoutComponent } from './containers/layout/layout.component'
 import { NotFoundComponent } from './containers/not-found/not-found.component'
 import { HeaderComponent } from './components/header/header.component'
+import { FooterComponent } from './components/footer/footer.component'
 
 @NgModule({
-  declarations: [NotFoundComponent, LayoutComponent, HeaderComponent],
+  declarations: [
+    NotFoundComponent,
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
   imports: [CommonModule, SharedRoutingModule],
   exports: [NotFoundComponent, LayoutComponent]
 })
