a4732ec1fa0b9e06264cd18249fd72f10d065baf
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2021-07-30 18:31:19 -0300

    ✨ Se creó el container "projects" en el módulo "admin"

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-
 import { AdminRoutingModule } from './admin-routing.module'
 import { HomeComponent } from './containers/home/home.component'
 import { AdminsComponent } from './containers/admins/admins.component'
 import { DonorsComponent } from './containers/donors/donors.component'
+import { ProjectsComponent } from './containers/projects/projects.component'
 
 @NgModule({
-  declarations: [HomeComponent, AdminsComponent, DonorsComponent],
+  declarations: [
+    HomeComponent,
+    AdminsComponent,
+    DonorsComponent,
+    ProjectsComponent
+  ],
   imports: [CommonModule, AdminRoutingModule]
 })
 export class AdminModule {}
