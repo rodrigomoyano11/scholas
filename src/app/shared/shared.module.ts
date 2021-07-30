6e55003613aafd06001a5bfdd80a4a8c019728ac
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2021-07-30 18:59:43 -0300

    ✨ Se creó el componente "header" en el módulo "shared"

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common'
 import { SharedRoutingModule } from './shared-routing.module'
 import { LayoutComponent } from './containers/layout/layout.component'
 import { NotFoundComponent } from './containers/not-found/not-found.component'
+import { HeaderComponent } from './components/header/header.component'
 
 @NgModule({
-  declarations: [NotFoundComponent, LayoutComponent],
+  declarations: [NotFoundComponent, LayoutComponent, HeaderComponent],
   imports: [CommonModule, SharedRoutingModule],
   exports: [NotFoundComponent, LayoutComponent]
 })
