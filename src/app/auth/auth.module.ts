d229c43679962a2b735dc202d4f2d9b06331efa9
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2021-07-30 18:12:51 -0300

    ✨ Se creó el container "forgot-password" en el módulo "auth"

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 
 import { AuthRoutingModule } from './auth-routing.module'
-import { RegisterComponent } from './containers/register/register.component';
+import { RegisterComponent } from './containers/register/register.component'
 import { LoginComponent } from './containers/login/login.component'
+import { ForgotPasswordComponent } from './containers/forgot-password/forgot-password.component'
 
 @NgModule({
-  declarations: [RegisterComponent, LoginComponent],
+  declarations: [RegisterComponent, LoginComponent, ForgotPasswordComponent],
   imports: [CommonModule, AuthRoutingModule]
 })
 export class AuthModule {}
