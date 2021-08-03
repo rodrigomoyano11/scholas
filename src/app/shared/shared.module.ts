3a0eafdca5002d49c8d54154a9f8e3ce84477880
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2021-08-03 08:26:16 -0300

    ✨ Se maquetó el componente "header"

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,6 +5,11 @@ import { LayoutComponent } from './containers/layout/layout.component'
 import { NotFoundComponent } from './containers/not-found/not-found.component'
 import { HeaderComponent } from './components/header/header.component'
 import { FooterComponent } from './components/footer/footer.component'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatMenuModule } from '@angular/material/menu'
+import { MatDividerModule } from '@angular/material/divider'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
 
 @NgModule({
   declarations: [
@@ -13,7 +18,15 @@ import { FooterComponent } from './components/footer/footer.component'
     HeaderComponent,
     FooterComponent
   ],
-  imports: [CommonModule, SharedRoutingModule],
+  imports: [
+    CommonModule,
+    SharedRoutingModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   exports: [NotFoundComponent, LayoutComponent]
 })
 export class SharedModule {}
