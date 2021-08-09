3d963cf8e5c0429c7b0a0eb6086d42a7d021d8c2
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2021-08-09 16:10:13 -0300

    ✨ Se implementó un título condicional al componente "header"

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -8,9 +8,23 @@ import { AuthService } from 'src/app/auth/services/auth/auth.service'
 })
 export class HeaderComponent {
   isLogged = false
+  claims!: { [key: string]: string }
+  userType!: 'Donante' | 'Administrador'
 
   constructor(private auth: AuthService) {
-    this.auth.user$.subscribe((user) => (this.isLogged = !!user?.getIdToken(true)))
+    this.auth.user$.subscribe(
+      (user) =>
+        void user?.getIdTokenResult().then((idTokenResult) => {
+          this.isLogged = !!idTokenResult.token
+          this.claims = idTokenResult.claims
+          this.selectTitle()
+        })
+    )
+  }
+
+  selectTitle(): void {
+    if (this.claims.admin) this.userType = 'Administrador'
+    this.userType = 'Donante'
   }
 
   logout(): void {
