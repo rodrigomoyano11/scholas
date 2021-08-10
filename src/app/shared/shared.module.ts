ba5147bec7954f97bafbfe8d002bfa6447ac3d2b
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2021-08-10 09:41:56 -0300

    ✨ Se creó e implementó el componente "logo" en el módulo "shared"

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -10,9 +10,16 @@ import { MatMenuModule } from '@angular/material/menu'
 import { MatDividerModule } from '@angular/material/divider'
 import { MatIconModule } from '@angular/material/icon'
 import { MatButtonModule } from '@angular/material/button'
+import { LogoComponent } from './components/logo/logo.component'
 
 @NgModule({
-  declarations: [NotFoundComponent, LayoutComponent, HeaderComponent, FooterComponent],
+  declarations: [
+    NotFoundComponent,
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    LogoComponent
+  ],
   imports: [
     CommonModule,
     SharedRoutingModule,
