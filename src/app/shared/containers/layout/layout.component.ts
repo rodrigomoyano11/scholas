0cfee3df4a1b31a5b6b4c4fd9db50abb6897aeab
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2021-08-11 07:07:08 -0300

    🔥 Se eliminó código innecesario

--- src/app/shared/containers/layout/layout.component.ts
+++ src/app/shared/containers/layout/layout.component.ts
@@ -1,21 +1,8 @@
-import { Component, OnInit } from '@angular/core'
-import { AuthService } from 'src/app/auth/services/auth/auth.service'
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css']
 })
-export class LayoutComponent implements OnInit {
-  isLogged = false
-  isEmailVerified = false
-
-  constructor(private auth: AuthService) {}
-
-  ngOnInit(): void {
-    this.auth.user$.subscribe((user) => {
-      this.isEmailVerified = !!user?.emailVerified
-      this.isLogged = !!user?.getIdToken(true)
-    })
-  }
-}
+export class LayoutComponent {}
