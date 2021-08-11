77414f3d62301eeaaf49cd8bcdd457ea0b7f7e41
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2021-08-11 07:50:06 -0300

    ♻️ Se refactorizó el componente "header"

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core'
+import { Component, OnDestroy } from '@angular/core'
+import { Subscription } from 'rxjs'
 import { AuthService } from 'src/app/auth/services/auth/auth.service'
 
 @Component({
@@ -6,32 +7,22 @@ import { AuthService } from 'src/app/auth/services/auth/auth.service'
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-  isLogged = false
-  claims!: { [key: string]: string }
+export class HeaderComponent implements OnDestroy {
+  authState!: Subscription
+
   userType!: 'Donante' | 'Administrador'
 
-  constructor(private auth: AuthService) {
-    this.auth.user$.subscribe(
-      (user) =>
-        void user?.getIdTokenResult().then((idTokenResult) => {
-          this.isLogged = !!idTokenResult.token
-          this.claims = idTokenResult.claims
-          this.selectTitle()
-        })
-    )
+  constructor(public auth: AuthService) {
+    this.authState = this.auth.authState$.subscribe(() => {
+      this.selectTitle()
+    })
   }
 
   selectTitle(): void {
-    if (this.claims.admin) this.userType = 'Administrador'
-    this.userType = 'Donante'
+    this.auth.claims?.admin ? (this.userType = 'Administrador') : (this.userType = 'Donante')
   }
 
-  logout(): void {
-    void this.auth.logout()
-  }
-
-  deleteAccount(): void {
-    void this.auth.deleteAccount()
+  ngOnDestroy(): void {
+    this.authState.unsubscribe()
   }
 }
