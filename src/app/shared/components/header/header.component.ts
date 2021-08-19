b4cd367838825a43bd5f690ab01feced9da09070
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2021-08-19 07:48:41 -0300

    ✨ Se implementó el comportamiento responsive a "header"

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,30 +1,12 @@
-import { Component, OnDestroy } from '@angular/core'
-import { Subscription } from 'rxjs'
+import { Component } from '@angular/core'
 import { AuthService } from 'src/app/auth/services/auth/auth.service'
+import { LayoutService } from '../../services/layout/layout.service'
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnDestroy {
-  authState!: Subscription
-
-  userType!: 'Donante' | 'Administrador' | string
-
-  constructor(public auth: AuthService) {
-    this.authState = this.auth.authState$.subscribe(() => {
-      this.selectTitle()
-    })
-  }
-
-  selectTitle(): void {
-    this.auth.claims?.admin
-      ? (this.userType = 'Administrador')
-      : (this.userType = this.auth.user?.displayName || 'Donante')
-  }
-
-  ngOnDestroy(): void {
-    this.authState.unsubscribe()
-  }
+export class HeaderComponent {
+  constructor(public auth: AuthService, public layout: LayoutService) {}
 }
