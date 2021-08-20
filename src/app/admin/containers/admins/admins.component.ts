2757eeba9309cfb31d65849fd09c99beb0ed8e79
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2021-08-20 15:00:32 -0300

    ✨ Se implementó el comportamiento responsive a "admins"

--- src/app/admin/containers/admins/admins.component.ts
+++ src/app/admin/containers/admins/admins.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { LayoutService } from 'src/app/shared/services/layout/layout.service'
 import { AdminsService } from '../../services/admins/admins.service'
 
 @Component({
   selector: 'app-admins',
   templateUrl: './admins.component.html',
   styleUrls: ['./admins.component.css']
-  // encapsulation: ViewEncapsulation.None
 })
 export class AdminsComponent implements OnInit {
-  constructor(public adminsService: AdminsService) {}
+  constructor(public adminsService: AdminsService, public layout: LayoutService) {}
 
   ngOnInit(): void {
     this.adminsService.getAdmins()
