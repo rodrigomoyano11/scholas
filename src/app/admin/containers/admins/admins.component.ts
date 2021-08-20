7cdcf883cdddac0df9f1342af57c10e7ee1fa2a1
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2021-08-20 14:56:39 -0300

    🎨 Se modificaron los estilos y estructura del componente "admins"

--- src/app/admin/containers/admins/admins.component.ts
+++ src/app/admin/containers/admins/admins.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, ViewEncapsulation } from '@angular/core'
 import { AdminsService } from '../../services/admins/admins.service'
 
 @Component({
   selector: 'app-admins',
   templateUrl: './admins.component.html',
   styleUrls: ['./admins.component.css']
+  // encapsulation: ViewEncapsulation.None
 })
 export class AdminsComponent implements OnInit {
   constructor(public adminsService: AdminsService) {}
