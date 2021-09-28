ff24f16d6c987cbe206647d6ec51d4b70c9e23e4
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2021-09-28 10:14:59 -0300

    ✨ Se creó la versión móvil del componente "image-gallery"

--- src/app/shared/components/image-gallery/image-gallery.component.ts
+++ src/app/shared/components/image-gallery/image-gallery.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnChanges } from '@angular/core'
+import { LayoutService } from '../../services/layout/layout.service'
 
 @Component({
   selector: 'app-image-gallery',
@@ -9,6 +10,8 @@ export class ImageGalleryComponent implements OnChanges {
   @Input() images: string[] = []
 
   imagesConverted: { path: string }[] = []
+
+  constructor(public layout: LayoutService) {}
 
   ngOnChanges(): void {
     this.images.map((image) => this.imagesConverted.push({ path: image }))
