a426a6ac3099bdae7cd3c1b32fd9926482c5aac4
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2021-10-05 10:58:01 -0300

    🐞 Se realizaron algunas correcciones generales en el componente "list-item"

--- src/app/shared/components/list-item/list-item.component.ts
+++ src/app/shared/components/list-item/list-item.component.ts
@@ -1,4 +1,10 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core'
+import { BadgeComponent } from '../badge/badge.component'
+
+export interface BadgeStatus {
+  type: BadgeComponent['status']
+  label: BadgeComponent['label']
+}
 
 @Component({
   selector: 'app-list-item',
@@ -12,4 +18,5 @@ export class ListItemComponent {
   @Input() subtitle!: string | null
   @Input() disabled = false
   @Input() actionItem!: boolean
+  @Input() status!: BadgeStatus | null
 }
