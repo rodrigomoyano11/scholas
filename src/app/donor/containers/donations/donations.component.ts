2301a2f517da8a9fb0a244bd0517b307ec54224f
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2021-10-05 06:52:44 -0300

    ✨ Se creó e implementó el servicio "donations"

--- src/app/donor/containers/donations/donations.component.ts
+++ src/app/donor/containers/donations/donations.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core'
 import { Router } from '@angular/router'
+import { DonationsService } from 'src/app/donation/services/donations/donations.service'
 import { ToolbarButtons } from 'src/app/shared/components/toolbar/toolbar.component'
+import { Donation } from 'src/app/shared/models/Donation'
 
 @Component({
   selector: 'app-donations',
@@ -8,64 +10,6 @@ import { ToolbarButtons } from 'src/app/shared/components/toolbar/toolbar.compon
   styleUrls: ['./donations.component.css'],
 })
 export class DonationsComponent {
-  donations = [
-    {
-      id: '1',
-      status: 'approved',
-      type: 'regular',
-      amount: 300,
-      createdDate: '2021-10-23',
-      userId: '30454656495658',
-      projectId: 1,
-      projectName: 'Hogar de Niños "Dr. Marcial Rawson"',
-      paymentId: '23475876',
-    },
-    {
-      id: '2',
-      status: 'approved',
-      type: 'regular',
-      amount: 300,
-      createdDate: '2017-09-08',
-      userId: '30454656495658',
-      projectId: 1,
-      projectName: 'Hospital de Oncología "Dr. René Favaloro"',
-      paymentId: '23475876',
-    },
-    {
-      id: '3',
-      status: 'approved',
-      type: 'regular',
-      amount: 300,
-      createdDate: '2021-12-15',
-      userId: '30454656495658',
-      projectId: 1,
-      projectName: 'Escuela de Mar y Playa',
-      paymentId: '23475876',
-    },
-    {
-      id: '4',
-      status: 'approved',
-      type: 'regular',
-      amount: 700,
-      createdDate: '2020-07-05',
-      userId: '89745394587099',
-      projectId: 2,
-      projectName: 'Comedero "Manuel Belgrano"',
-      paymentId: '75460977',
-    },
-    {
-      id: '5',
-      status: 'approved',
-      type: 'regular',
-      amount: 700,
-      createdDate: '2021-03-26',
-      userId: '89347935023745',
-      projectId: 3,
-      projectName: 'Scholas San Juan',
-      paymentId: '897349802',
-    },
-  ]
-
   toolbarButtons: ToolbarButtons = [
     {
       style: 'secondary',
@@ -84,9 +28,9 @@ export class DonationsComponent {
 
   backButtonAction = (): void => void this.router.navigate(['/auth/account'])
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, public donations: DonationsService) {}
 
-  goToDonationDetails(): void {
-    console.log('Works')
+  goToDonationDetails(id: Donation['id']): void {
+    void this.router.navigate(['/donation/certificate', id])
   }
 }
