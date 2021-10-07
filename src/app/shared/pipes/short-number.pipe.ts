0d53ea0bb6c162f9398f347988c5c504d81ccf61
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2021-10-07 09:33:20 -0300

    🐞 Se intercambió el uso de "." por la "," en la pipe "short-number"

--- src/app/shared/pipes/short-number.pipe.ts
+++ src/app/shared/pipes/short-number.pipe.ts
@@ -4,24 +4,26 @@ import { Pipe, PipeTransform } from '@angular/core'
   name: 'shortNumber',
 })
 export class ShortNumberPipe implements PipeTransform {
-  transform(number: number): string | null {
+  transform(number: number | string | null): string | null {
     // Verify types
+    if (number === null || number === undefined) return null
+    if (typeof number === 'string') return null
     if (isNaN(number)) return null
-    if (number === null) return null
     if (number === 0) return null
 
     let absoluteNumber = Math.abs(number)
-    const rounder = Math.pow(10, 1)
+    const rounder = 10 ** 1
     const isNegative = number < 0
 
     let key = ''
 
     const powers = [
-      { key: 'Q', value: Math.pow(10, 15) },
-      { key: 'T', value: Math.pow(10, 12) },
-      { key: 'B', value: Math.pow(10, 9) },
-      { key: 'M', value: Math.pow(10, 6) },
-      { key: 'K', value: 1000 },
+      { key: 'Q', value: 10 ** 24 },
+      { key: 'T', value: 10 ** 18 },
+      { key: 'B', value: 10 ** 12 },
+      { key: 'MM', value: 10 ** 9 },
+      { key: 'M', value: 10 ** 6 },
+      { key: 'K', value: 10 ** 3 },
     ]
 
     for (let i = 0; i < powers.length; i++) {
@@ -33,6 +35,6 @@ export class ShortNumberPipe implements PipeTransform {
         break
       }
     }
-    return `${isNegative ? '-' : ''}${absoluteNumber}${key}`
+    return `${isNegative ? '-' : ''}${absoluteNumber} ${key}`.replace('.', ',')
   }
 }
