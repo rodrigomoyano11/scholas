bf8abcf1114d2494bfcca2998ead5b2ff294b144
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2021-10-07 07:31:54 -0300

    ✨ Se maquetó e implementó el componente "metrics"

--- src/app/admin/containers/metrics/metrics.component.ts
+++ src/app/admin/containers/metrics/metrics.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { Router } from '@angular/router'
+import { CreateProjectResponse } from 'src/app/shared/models/api.interface'
+import { Project } from 'src/app/shared/models/project.interface'
+import { ProjectsService } from 'src/app/shared/services/projects/projects.service'
 
 @Component({
   selector: 'app-metrics',
   templateUrl: './metrics.component.html',
   styleUrls: ['./metrics.component.css'],
 })
-export class MetricsComponent {}
+export class MetricsComponent implements OnInit {
+  projectData: CreateProjectResponse[] = []
+
+  constructor(private router: Router, public projects: ProjectsService) {}
+
+  ngOnInit(): void {
+    this.projects
+      .getProjects()
+      .subscribe((projects) => projects.forEach((project) => this.projectData.push(project)))
+  }
+
+  goToMetrics(id: Project['id']): void {
+    void this.router.navigate(['/admin/projects/read/', id])
+  }
+}
