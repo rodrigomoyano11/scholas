8ed21c2386418a5df5aa3638012b565b63553bf7
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2021-10-28 08:21:36 -0300

    ✨ Se agregó un dialog a la guard "is-admin"

--- src/app/shared/guards/isAdmin/is-admin.guard.ts
+++ src/app/shared/guards/isAdmin/is-admin.guard.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core'
+import { MatDialog } from '@angular/material/dialog'
 import { CanActivate, CanActivateChild, CanLoad, Router, UrlTree } from '@angular/router'
 import { Observable } from 'rxjs'
 import { AuthService } from 'src/app/auth/services/auth/auth.service'
+import { DialogComponent, DialogData } from '../../components/dialog/dialog.component'
 
 type GuardResult = Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree
 
@@ -9,7 +11,7 @@ type GuardResult = Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | 
   providedIn: 'root',
 })
 export class IsAdminGuard implements CanActivate, CanActivateChild, CanLoad {
-  constructor(private auth: AuthService, private router: Router) {}
+  constructor(private auth: AuthService, private router: Router, private dialog: MatDialog) {}
 
   // General
   async hasPermissions(): Promise<boolean> {
@@ -17,7 +19,21 @@ export class IsAdminGuard implements CanActivate, CanActivateChild, CanLoad {
       const conditions = await Promise.all([this.auth.userIsLogged(), this.auth.userIsAdmin()])
 
       const response = conditions.every((condition) => condition === true)
-      if (!response) void this.router.navigate(['/auth/login'])
+      if (!response) {
+        void this.router.navigate(['/auth/login'])
+        void this.dialog
+          .open<DialogComponent, DialogData>(DialogComponent, {
+            data: {
+              actions: [null, 'Cerrar'],
+              title: 'Importante',
+              description:
+                'Debes iniciar sesión con una cuenta de administrador para poder ingresar',
+              icon: 'info',
+            },
+          })
+          .afterClosed()
+          .toPromise()
+      }
       return response
     } catch {
       return false
