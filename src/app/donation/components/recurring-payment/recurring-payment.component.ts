4b9c777a0fdbb59157c6b9d2775e36efe15992c7
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2021-11-08 01:07:00 -0300

    ✨ Se implementaron valores dinámicos en los componentes "recurring-payment" y "success-payment"

--- src/app/donation/components/recurring-payment/recurring-payment.component.ts
+++ src/app/donation/components/recurring-payment/recurring-payment.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core'
+import { AuthService } from 'src/app/auth/services/auth/auth.service'
 
 @Component({
   selector: 'app-recurring-payment',
   templateUrl: './recurring-payment.component.html',
   styleUrls: ['./recurring-payment.component.css'],
 })
-export class RecurringPaymentComponent {}
+export class RecurringPaymentComponent {
+  constructor(public auth: AuthService) {}
+}
--- src/app/donation/components/success-payment/success-payment.component.ts
+++ src/app/donation/components/success-payment/success-payment.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core'
+import { AuthService } from 'src/app/auth/services/auth/auth.service'
 
 @Component({
   selector: 'app-success-payment',
   templateUrl: './success-payment.component.html',
   styleUrls: ['./success-payment.component.css'],
 })
-export class SuccessPaymentComponent {}
+export class SuccessPaymentComponent {
+  constructor(public auth: AuthService) {}
+}
